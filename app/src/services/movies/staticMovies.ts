5d54d6023bcf943eda35e891d4d24dd8d2ed0d41
Matthew Wong <mattckwong@gmail.com>
2025-12-05 16:36:24 -0500

    Seed static movie data with trailers

--- app/src/services/movies/staticMovies.ts
+++ app/src/services/movies/staticMovies.ts
@@ -1,19 +1,255 @@
 import { Media } from '@/types/media';
 
-const staticMovies: Media[] = [
+export const staticMovies: Media[] = [
   {
-    id: 'static-1',
+    id: 'tt0111161',
     mediaType: 'movie',
-    title: 'Static Test Movie 1',
-    overview: 'Test overview',
-    genres: ['Action'],
-    directors: ['Jane Doe'],
-    cast: ['Actor A'],
-    streamingOptions: [],
-    releaseYear: 2020,
-    runtime: 120,
-    poster: '',
-    trailerUrl: 'https://www.youtube.com/embed/dQw4w9WgXcQ',
+    title: 'The Shawshank Redemption',
+    overview:
+      'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
+    genres: ['Drama'],
+    directors: ['Frank Darabont'],
+    cast: ['Tim Robbins', 'Morgan Freeman', 'Bob Gunton'],
+    streamingOptions: [
+      {
+        countryCode: 'us',
+        services: [
+          {
+            serviceName: 'Netflix',
+            logo: 'https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg',
+          },
+        ],
+      },
+    ],
+    releaseYear: 1994,
+    runtime: 142,
+    poster: 'https://image.tmdb.org/t/p/w500/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg',
+    trailerUrl: 'https://www.youtube.com/embed/6hB3S9bIaco',
+  },
+  {
+    id: 'tt0468569',
+    mediaType: 'movie',
+    title: 'The Dark Knight',
+    overview:
+      'When the menace known as the Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham.',
+    genres: ['Action', 'Crime', 'Drama'],
+    directors: ['Christopher Nolan'],
+    cast: ['Christian Bale', 'Heath Ledger', 'Aaron Eckhart'],
+    streamingOptions: [
+      {
+        countryCode: 'us',
+        services: [
+          {
+            serviceName: 'HBO Max',
+            logo: 'https://upload.wikimedia.org/wikipedia/commons/1/17/HBO_Max_Logo.svg',
+          },
+        ],
+      },
+    ],
+    releaseYear: 2008,
+    runtime: 152,
+    poster: 'https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg',
+    trailerUrl: 'https://www.youtube.com/embed/EXeTwQWrcwY',
+  },
+  {
+    id: 'tt0137523',
+    mediaType: 'movie',
+    title: 'Fight Club',
+    overview:
+      "An insomniac office worker and a devil-may-care soapmaker form an underground fight club that evolves into much more.",
+    genres: ['Drama'],
+    directors: ['David Fincher'],
+    cast: ['Brad Pitt', 'Edward Norton', 'Meat Loaf'],
+    streamingOptions: [
+      {
+        countryCode: 'us',
+        services: [
+          {
+            serviceName: 'Amazon Prime Video',
+            logo: 'https://upload.wikimedia.org/wikipedia/commons/f/f1/Prime_Video.png',
+          },
+        ],
+      },
+    ],
+    releaseYear: 1999,
+    runtime: 139,
+    poster: 'https://image.tmdb.org/t/p/w500/bptfVGEQuv6vDTIMVCHjJ9Dz8PX.jpg',
+    trailerUrl: 'https://www.youtube.com/embed/SUXWAEX2jlg',
+  },
+  {
+    id: 'tt0109830',
+    mediaType: 'movie',
+    title: 'Forrest Gump',
+    overview:
+      'The presidencies of Kennedy and Johnson, Vietnam, Watergate and other history unfold through the perspective of an Alabama man with an IQ of 75.',
+    genres: ['Drama', 'Romance'],
+    directors: ['Robert Zemeckis'],
+    cast: ['Tom Hanks', 'Robin Wright', 'Gary Sinise'],
+    streamingOptions: [
+      {
+        countryCode: 'us',
+        services: [
+          {
+            serviceName: 'Paramount+',
+            logo: 'https://upload.wikimedia.org/wikipedia/commons/7/75/Paramount%2B_Logo.svg',
+          },
+        ],
+      },
+    ],
+    releaseYear: 1994,
+    runtime: 142,
+    poster: 'https://image.tmdb.org/t/p/w500/saHP97rTPS5eLmrLQEcANmKrsFl.jpg',
+    trailerUrl: 'https://www.youtube.com/embed/uPIEn0M8su0',
+  },
+  {
+    id: 'tt1375666',
+    mediaType: 'movie',
+    title: 'Inception',
+    overview:
+      'A thief who steals corporate secrets through use of dream-sharing technology is given the inverse task of planting an idea into the mind of a CEO.',
+    genres: ['Action', 'Sci-Fi', 'Thriller'],
+    directors: ['Christopher Nolan'],
+    cast: ['Leonardo DiCaprio', 'Joseph Gordon-Levitt', 'Elliot Page'],
+    streamingOptions: [
+      {
+        countryCode: 'us',
+        services: [
+          {
+            serviceName: 'Netflix',
+            logo: 'https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg',
+          },
+        ],
+      },
+    ],
+    releaseYear: 2010,
+    runtime: 148,
+    poster: 'https://image.tmdb.org/t/p/w500/9gk7adHYeDvHkCSEqAvQNLV5Uge.jpg',
+    trailerUrl: 'https://www.youtube.com/embed/YoHD9XEInc0',
+  },
+  {
+    id: 'tt0088763',
+    mediaType: 'movie',
+    title: 'Back to the Future',
+    overview:
+      'Marty McFly, a 17-year-old high school student, is accidentally sent thirty years into the past in a plutonium-powered time-traveling DeLorean invented by his close friend, the eccentric scientist Doc Brown.',
+    genres: ['Adventure', 'Comedy', 'Sci-Fi'],
+    directors: ['Robert Zemeckis'],
+    cast: ['Michael J. Fox', 'Christopher Lloyd', 'Lea Thompson'],
+    streamingOptions: [
+      {
+        countryCode: 'us',
+        services: [
+          {
+            serviceName: 'Peacock',
+            logo: 'https://upload.wikimedia.org/wikipedia/commons/3/32/Peacock_Logo.svg',
+          },
+        ],
+      },
+    ],
+    releaseYear: 1985,
+    runtime: 116,
+    poster: 'https://image.tmdb.org/t/p/w500/pTpxQB1N0waaSc3OSn0e9oc8kx9.jpg',
+    trailerUrl: 'https://www.youtube.com/embed/qvsgGtivCgs',
+  },
+  {
+    id: 'tt0110912',
+    mediaType: 'movie',
+    title: 'Pulp Fiction',
+    overview:
+      'The lives of two mob hitmen, a boxer, a gangster’s wife, and a pair of diner bandits intertwine in four tales of violence and redemption.',
+    genres: ['Crime', 'Drama'],
+    directors: ['Quentin Tarantino'],
+    cast: ['John Travolta', 'Samuel L. Jackson', 'Uma Thurman'],
+    streamingOptions: [
+      {
+        countryCode: 'us',
+        services: [
+          {
+            serviceName: 'Hulu',
+            logo: 'https://upload.wikimedia.org/wikipedia/commons/e/e4/Hulu_Logo.svg',
+          },
+        ],
+      },
+    ],
+    releaseYear: 1994,
+    runtime: 154,
+    poster: 'https://image.tmdb.org/t/p/w500/dM2w364MScsjFf8pfMbaWUcWrR.jpg',
+    trailerUrl: 'https://www.youtube.com/embed/s7EdQ4FqbhY',
+  },
+  {
+    id: 'tt0120737',
+    mediaType: 'movie',
+    title: 'The Lord of the Rings: The Fellowship of the Ring',
+    overview:
+      'A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron.',
+    genres: ['Adventure', 'Drama', 'Fantasy'],
+    directors: ['Peter Jackson'],
+    cast: ['Elijah Wood', 'Ian McKellen', 'Orlando Bloom'],
+    streamingOptions: [
+      {
+        countryCode: 'us',
+        services: [
+          {
+            serviceName: 'Amazon Prime Video',
+            logo: 'https://upload.wikimedia.org/wikipedia/commons/f/f1/Prime_Video.png',
+          },
+        ],
+      },
+    ],
+    releaseYear: 2001,
+    runtime: 178,
+    poster: 'https://image.tmdb.org/t/p/w500/6oom5QYQ2yQTMJIbnvbkBL9cHo6.jpg',
+    trailerUrl: 'https://www.youtube.com/embed/V75dMMIW2B4',
+  },
+  {
+    id: 'tt0099685',
+    mediaType: 'movie',
+    title: 'Goodfellas',
+    overview:
+      'The story of Henry Hill and his life in the mob, covering his relationship with his wife Karen Hill and his mob partners over three decades.',
+    genres: ['Biography', 'Crime', 'Drama'],
+    directors: ['Martin Scorsese'],
+    cast: ['Robert De Niro', 'Ray Liotta', 'Joe Pesci'],
+    streamingOptions: [
+      {
+        countryCode: 'us',
+        services: [
+          {
+            serviceName: 'Max',
+            logo: 'https://upload.wikimedia.org/wikipedia/commons/1/17/HBO_Max_Logo.svg',
+          },
+        ],
+      },
+    ],
+    releaseYear: 1990,
+    runtime: 146,
+    poster: 'https://image.tmdb.org/t/p/w500/aKuFiU82s5ISJpGZp7YkIr3kCUd.jpg',
+    trailerUrl: 'https://www.youtube.com/embed/2ilzidi_J8Q',
+  },
+  {
+    id: 'tt0120815',
+    mediaType: 'movie',
+    title: 'Saving Private Ryan',
+    overview:
+      'Following the Normandy Landings, Captain John H. Miller and his men must go behind enemy lines to find and bring home the titular Private Ryan.',
+    genres: ['Drama', 'War'],
+    directors: ['Steven Spielberg'],
+    cast: ['Tom Hanks', 'Matt Damon', 'Tom Sizemore'],
+    streamingOptions: [
+      {
+        countryCode: 'us',
+        services: [
+          {
+            serviceName: 'Paramount+',
+            logo: 'https://upload.wikimedia.org/wikipedia/commons/7/75/Paramount%2B_Logo.svg',
+          },
+        ],
+      },
+    ],
+    releaseYear: 1998,
+    runtime: 169,
+    poster: 'https://image.tmdb.org/t/p/w500/miDoEMlYDJhOCvxlzI0wZqBs9Yt.jpg',
+    trailerUrl: 'https://www.youtube.com/embed/zwhP5b4tD6g',
   },
 ];
 
